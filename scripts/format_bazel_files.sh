ec87e4af13dd50e01fd5eb88a52d721dd244f3b5
Chengnian Sun <chengniansun@gmail.com>
2024-05-02 11:54:07 -0400

    add different folders

---
+++ scripts/format_bazel_files.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+readonly SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "${SCRIPT_DIR}/constants.sh" || exit 1
+readonly TOOL="@com_github_bazelbuild_buildtools//:buildifier"
+
+# Using load("@com_github_bazelbuild_buildtools//buildifier:def.bzl", "buildifier")
+# does not work if we have submodules that are very large and contain BUILD files.
+# We need to explicitly specify which directories to format BUILD files.
+# bazelisk run //:buildifier
+
+bazelisk run "${TOOL}" -- ${ABS_BAZEL_DIRS_STRING} \
+  "${WORKSPACE_ROOT}/BUILD" \
+  "${WORKSPACE_ROOT}/WORKSPACE"
